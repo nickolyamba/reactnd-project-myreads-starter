94a526202feb7877a5eb0ead1371e95f49e3c91d
nickolyamba <nickolyamba@yahoo.com>
2017-08-20 23:04:23 -0700

    feat: Create two components Header and Search

---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = (props) => (
+    <div>Header</div>
+
+);
---
+++ src/Search.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Search = (props) => (
+    <div>Search</div>
+);
