46574c8d4ada7eb17d23bbe7be83e4a8d3c5e984
nickolyamba <nickolyamba@yahoo.com>
2017-08-25 21:53:03 -0700

    feature: Populate books in state with data from BookAPI. Create shelf types objects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
 import './App.css'
 import {Route} from 'react-router-dom'
 import BookLibrary from './BookLibrary';
 import Search from "./Search";
+import * as BooksAPI from './BooksAPI'
+
+const shelfTypes = {
+        currentlyReading: {value: 'currentlyReading', name: 'Currently Reading'},
+        wantToRead: {value: 'wantToRead', name: 'Want to Read'},
+        read: {value: 'read', name: 'Read'},
+        none: {value: 'none', name: 'None'}
+    };
 
 class BooksApp extends React.Component {
   state = {
-      currentRead: [],
-      wantToRead: [],
-      read: []
+      books: []
   };
+
+  componentDidMount(){
+      BooksAPI.getAll().then(books => (
+          this.setState({books})
+      ));
+  }
 
   render() {
     return (
       <div className="app">
           {/*BookLibrary Component*/}
           <Route exact path="/" render={() => (
-              <BookLibrary/>
+              this.state.books &&
+              <BookLibrary books={this.state.books} shelfTypes={shelfTypes}/>
             )}
           />
+
           {/*Search Component*/}
           <Route path="/search" render={({history}) => (
               <Search/>
