e58e27ab6c9810f5a47e791dbad3a039b6b3019d
nickolyamba <nickolyamba@yahoo.com>
2017-08-26 12:58:21 -0700

    feature: Pass default value to shelf changer

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -4,18 +4,23 @@ import {shelfTypes, shelfChangerHeader} from './config';
 
 class ShelfChanger extends React.Component{
     render(){
+        const selectedOption = this.props.selectedOption;
         return(
             //  the default value for the control should always be the current shelf the book is in.
             <div className="book-shelf-changer">
-                <select>
+                <select defaultValue={selectedOption}>
                     <option key={shelfChangerHeader} value="none" disabled>{shelfChangerHeader}</option>
                     {shelfTypes.map(shelf => (
-                        <option key={shelf.value} value={shelf.value}>{shelf.name}</option>
+                        <option key={shelf.id} value={shelf.id}>{shelf.name}</option>
                     ))}
                 </select>
             </div>
         )
     }
 }
+
+ShelfChanger.propTypes = {
+    selectedOption: PropTypes.string.isRequired,
+};
 
 export default ShelfChanger;
